872592f94018da5681c4ff841a8840915344c5b7
ObO #@-@ <obo-@ObOs-MBP.hub>
2020-10-30 16:23:15 +1300

    upload

---
+++ webpage.js
@@ -0,0 +1,57 @@
+//Johnny Yang
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+var i = 0;
+
+function read(){
+    if(!i){
+        document.getElementById("more").style.display = "inline";
+        document.getElementById("dots").style.display = "none";
+        document.getElementById("read").innerHTML = "Read less";
+        i = 1;
+    }else{
+        document.getElementById("more").style.display = "none";
+        document.getElementById("dots").style.display = "inline";
+        document.getElementById("read").innerHTML = "Read more";
+        i = 0;
+    }
+}
+//Johnny Yang
+// Isaiah
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+
+
+
